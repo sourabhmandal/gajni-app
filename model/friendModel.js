20783c8c02bf3e6c2ecef9fad493c02ca7c4cf6d
sourabhmandal <19mandal97@gmail.com>
2021-03-05 21:56:08 +0530

    improve api

--- model/friendModel.js
+++ model/friendModel.js
@@ -2,10 +2,9 @@ const mongoose = require("mongoose");
 
 const friendSchema = new mongoose.Schema({
   primary_photo_url: { type: String, required: true },
-  secondary_photo: [{ photos: String }],
   name: { type: String, required: true },
   description: {type: Array},
   date: { type: Date, default: Date.now },
-  place: String,
+  place: {type:String},
 });
 module.exports = mongoose.model("Friend", friendSchema);
